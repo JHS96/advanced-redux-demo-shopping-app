a3b9654c29df24c37b1b93b2cf7e6984a1300c7e
JHS96 <jschaper@hotmail.co.za>
2026-01-30 14:06:59 +0200

    Redux Toolkit set up. Toggle cart implemented

--- src/components/Layout/MainHeader.js
+++ src/components/Layout/MainHeader.js
@@ -1,14 +1,23 @@
+import { useDispatch } from 'react-redux';
+
 import CartButton from '../Cart/CartButton';
 import classes from './MainHeader.module.css';
+import { cartActions } from '../../store/cart';
 
 const MainHeader = (props) => {
+  const dispatch = useDispatch();
+
+  function handleToggleCart() {
+    dispatch(cartActions.toggleCart());
+  }
+
   return (
     <header className={classes.header}>
       <h1>ReduxCart</h1>
       <nav>
         <ul>
           <li>
-            <CartButton />
+            <CartButton handleClick={handleToggleCart} />
           </li>
         </ul>
       </nav>
